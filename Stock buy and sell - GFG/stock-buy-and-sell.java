3625234abe174674cbac403225666e6a983aea59
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-03-02 16:47:12 +0530

    Added solution - LeetHub

--- Stock buy and sell - GFG/stock-buy-and-sell.java
+++ Stock buy and sell - GFG/stock-buy-and-sell.java
@@ -1,39 +1,55 @@
 //{ Driver Code Starts
-// Initial Template for Java
+//Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
+public class GFG {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
             int n = Integer.parseInt(br.readLine().trim());
-            int[] price = new int[n];
-            String[] str = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                price[i] = Integer.parseInt(str[i]);
+            int A[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                A[i] = Integer.parseInt(inputLine[i]);
             }
-            Solution sln = new Solution();
-            sln.stockBuySell(price, n);
+            int p = 0;
+            for(int i=0; i<n-1; i++)
+                p += Math.max(0,A[i+1]-A[i]);
+                
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer> > ans = obj.stockBuySell(A, n);
+            if(ans.size()==0)
+                System.out.print("No Profit");
+            else{
+                int c=0;
+                for(int i=0; i<ans.size(); i++)
+                    c += A[ans.get(i).get(1)]-A[ans.get(i).get(0)];
+                
+                if(c==p)
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+            }System.out.println();
         }
     }
 }
 // } Driver Code Ends
 
 
-// User function Template for Java
+//User function Template for Java
 
-class Solution {
-    public void stockBuySell(int[] arr, int n) {
+class Solution{
+    //Function to find the days of buying and selling stock for max profit.
+    ArrayList<ArrayList<Integer> > stockBuySell(int arr[], int n) {
         // code here
-        int start =0;
+        
+         int start =0;
         int end = 0;
         int i =1;
+         ArrayList<ArrayList<Integer> > list = new ArrayList<>();
         boolean check = true;
         while(i<arr.length){
             if(arr[i]>arr[start] && arr[i]>arr[i-1]){
@@ -42,7 +58,11 @@ class Solution {
             else{
                 if(start!=end){
                     check = false;
-                    System.out.print("("+ start + " " + end +")" + " " );
+                    ArrayList<Integer> l = new ArrayList<>();
+                    l.add(start);
+                    l.add(end);
+                    list.add(l);
+                    // System.out.print("("+ start + " " + end +")" + " " );
                 }
                 start = i;
                 end =i;
@@ -51,12 +71,17 @@ class Solution {
         }
         if(start!=end){
             check = false;
-            System.out.print("("+ start + " " + end + ")"+ " ");
+             ArrayList<Integer> l = new ArrayList<>();
+                    l.add(start);
+                    l.add(end);
+                    list.add(l);
+            // System.out.print("("+ start + " " + end + ")"+ " ");
         }
-        if(check){
-            System.out.print("No Profit");
-        }
-        System.out.println("");
+        // if(check){
+        //     System.out.print("No Profit");
+        // }
+        // System.out.println("");
+        return list;
         
     }
 }
