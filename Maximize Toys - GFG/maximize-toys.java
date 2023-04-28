26dc15f4266e4e560cd9292b40ccf07ad1609a99
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-04-28 20:05:11 +0530

    Added solution - LeetHub

--- Maximize Toys - GFG/maximize-toys.java
+++ Maximize Toys - GFG/maximize-toys.java
@@ -33,6 +33,11 @@ class Solution{
     {
         // code here
         //date : 28th april 2023
+        // ! seccond approach to this solution can be by implementing a priority queue(min heap)
+        // we first make the PQ and then pop the topmost item as long as the item < sum and if it is,
+        // then we update the sum as sum - pq.poll(). Also, we can further optimise this solution by
+        // not making an external PQ and instead buliding a heap in the OG array itself just like we 
+        //did in heap sort algorithm ie, creating a buildheap fn and a minHeapify fn as well.
         Arrays.sort(arr);
         int count =0;
         int sum = 0;
