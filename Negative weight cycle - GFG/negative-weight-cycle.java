b35574a6861d2bc4c3cdda2a665cebdfd267574f
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-05-16 13:13:13 +0530

    Added solution - LeetHub

--- Negative weight cycle - GFG/negative-weight-cycle.java
+++ Negative weight cycle - GFG/negative-weight-cycle.java
@@ -38,12 +38,14 @@ class Solution
     public int isNegativeWeightCycle(int n, int[][] edges)
     {
         //code here
+        //date : 16th may 2023
+        //bellman ford algo to find negative cycle in the graph
+        //TC : O(V*E)
         int [] dist = new int[n];
         Arrays.fill(dist, (int)1e8);
         dist[0] = 0;
-        
+        //doing n-1 traversals to find the shortest path of each node from root 0
         for(int i =0; i<n-1; i++){
-            //   System.out.println("yaha eroor at"+ i);
             for( int [] temp : edges){
                 int u = temp[0];
                 int v = temp[1];
@@ -54,16 +56,17 @@ class Solution
                 
             }
         }
-        
+        //then doing the nth traversal to check for a negative cycle
+        //pls note that dist[u]!=1e8 condition isnt being used in this iteration as in n-1 traversals
+        //each vertex has been assigned with its shortest path already and this nth iteration checks for 
+        //negative cycle in the graph
        for( int [] temp : edges){
                 int u = temp[0];
                 int v = temp[1];
                 int wt = temp[2];
                 if(  dist[v]>dist[u] + wt){
-                    // dist[v] = dist[u] + wt;
                     return 1;
                 }
-                
             }
         return 0;
         
