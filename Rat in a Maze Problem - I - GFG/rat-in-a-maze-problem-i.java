0a34a9b7a1d58b49c0a45af2dbcf8ce25399e668
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-05-28 20:15:32 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
@@ -47,26 +47,27 @@ class Solution {
         return ans;
     }
     static void rmp(int i , int j, String res, ArrayList<String> ans, int [][] mat, int n, boolean vis [][]){
-        //base condition to check if we have reached the destination ie mat[n-1][j-1]( last cell)
        
-        
         if(!checkForSafeCell(i, j, mat, vis, n)){
-             if(i== n-1 && j==n-1){
-            ans.add(res);
-            return;
-        }
+            //base condition to check if we have reached the destination ie mat[n-1][j-1]( last cell)
+            if(i== n-1 && j== n-1){
+                ans.add(res);
+                return;
+            }
+            
             vis[i][j] = true;
             
             rmp(i-1, j, res + 'U', ans, mat, n, vis);
-            rmp(i, j+1, res + 'R', ans, mat, n, vis);
             rmp(i+1, j, res + 'D', ans, mat, n, vis);
-             rmp(i, j-1, res + 'L', ans, mat, n, vis);
+            rmp(i, j-1, res + 'L', ans, mat, n, vis);
+            rmp(i, j+1, res + 'R', ans, mat, n, vis);
+           
             
             vis[i][j] = false;
         }
         return;
     }
    static boolean   checkForSafeCell(int i, int j, int [][] mat, boolean [][] vis, int n ){
-        return (i<0 || j<0 || i>=n || j>=n || mat[i][j]==0 || vis[i][j] );
+        return (i<0 || j<0 || i>=n || j>=n || vis[i][j] || mat[i][j]==0);
     }
 }
