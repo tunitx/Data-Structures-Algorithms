f3f1a3088f53abbfbb406e86dfae44832b5d531f
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-06-09 19:22:11 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.java
+++ Form a palindrome - GFG/form-a-palindrome.java
@@ -34,7 +34,7 @@ class Solution{
                 dp[i][j]= -1;
             }
         }
-        return rev.length()- helper(S, rev, S.length(), S.length(), dp);
+        return rev.length() - helper(S, rev, S.length(), S.length(), dp);
     }
     int helper(String s1, String s2, int x, int y, int [][] dp){
         //base cases
@@ -43,11 +43,12 @@ class Solution{
         if(dp[x][y]!=-1) return dp[x][y];
         else{
         if(s1.charAt(x-1) == s2.charAt(y-1)){
-            dp[x][y] = 1+ helper(s1, s2, x-1, y-1, dp);
+            dp[x][y] = 1+  helper(s1, s2, x-1, y-1, dp);
         }
         else{
-          
-            dp[x][y] = Math.max( helper(s1, s2, x, y-1, dp), helper(s1, s2, x-1, y, dp));
+          int a =  helper(s1, s2, x, y-1, dp);
+          int b =  helper(s1, s2, x-1, y, dp);
+            dp[x][y] =  Math.max( a,b);
         }
         }
         return dp[x][y];
